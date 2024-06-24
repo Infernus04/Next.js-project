08e81f8a5dbf62aeab931ae98df9aa043d0792e4
Ishan <magardeishan@gmail.com>
2024-06-24 23:56:38 +0530

    setting up next auth

---
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -0,0 +1,52 @@
+import { NextAuthOptions } from "next-auth";
+import  CredentialsProvider  from "next-auth/providers/credentials";
+import brcypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/User";
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials : any) : Promise<any>{
+        await dbConnect()
+        try{
+            const user = await UserModel.findOne({
+                $or: [
+                    {email : credentials.identifier},
+                    {username : credentials.identifier}
+                ]
+            })
+            if(!user){
+                throw new Error("No user found with this email")
+            }
+
+            if(!user.isVerified){
+                throw new Error("Please verify your account before login")
+            }
+            const isPasswordCorret = await brcypt.compare(credentials.password, user.password)
+
+            if(isPasswordCorret){
+              return user
+            }else{
+              throw new Error("Incorrect password")
+            }
+
+        }catch(err : any){
+            throw new Error
+        }
+      }
+    }),
+  ],
+  pages : {
+    signIn : "/sign-in"
+  },
+  session : {
+    strategy : "jwt"
+  },
+  secret : process.env.NEXTAUTH_SECRET
+};
