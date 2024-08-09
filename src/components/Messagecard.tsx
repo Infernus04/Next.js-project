8760d27995075609c7d0e79a09681b0f4c294b9d
Ishan <magardeishan@gmail.com>
2024-08-09 17:24:44 +0530

    message card

---
+++ src/components/Messagecard.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog"
+  
+
+const Messagecard = () => {
+  
+};
+
+export default Messagecard;
